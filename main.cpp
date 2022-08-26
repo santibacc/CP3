278b37622a825dadcf3fdacd1e332dd5b7a49aea
sant1ago <bacconsanti@gmail.com>
2022-08-26 00:14:44 -0300

    Bitwisde

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
 #include "mbed.h"
-#define TITI 100
-#define WAIT 1500
 DigitalOut LED(PC_13);
 Timer myTimer;
 
 int main(){
 
     myTimer.start();
-    uint32_t counter=0;
-    uint32_t intervalo,count=0;
-    intervalo=TITI;
-
+    uint32_t intervalo=100,counter=0;
+    uint32_t mask=  0xA8000000;
+    uint32_t move; 
     while(1){
         
         if((myTimer.read_ms()-counter)>intervalo){
             counter=myTimer.read_ms();
             
-            count++;
-            if(count<6){
-                LED=!LED;
-            }else
+            if(mask<<move == 1){
+                LED=0;
+                move++;
+            }else{
                 LED=1;
-            if(count>19)
-                count=0;
+                move++;
+            }
         }
     }
 }
